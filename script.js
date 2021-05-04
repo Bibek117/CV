931052b9e984bbd832614fb0ec7dcc8657b5fc94
Bibek_16 <71329649+Bibek117@users.noreply.github.com>
2021-05-04 21:20:22 +0545

    final

--- script.js
+++ script.js
@@ -6,27 +6,32 @@ const aside = document.querySelector('aside');
 const html = document.querySelector('body');
 const foot = document.querySelector('footer');
 const icon = document.querySelectorAll('.icon');
+const icon1 =document.querySelectorAll('.fas');
 
 
 moon.addEventListener('click',()=>{
     moon.style.right = "-15px";
     sun.style.left = "4px";
-    sun.style.color = "white";
+   
     section.classList.toggle('active-section');
     aside.classList.toggle('active-aside');
     html.style.backgroundColor ="#fff";
     icon.forEach((value)=>{value.style.color ="black";});
+    icon1.forEach((value)=>{value.style.color ="black";});
     foot.classList.toggle('active-footer');
     holder.style.backgroundColor ="black";
+     sun.style.color = "white";
 })
 sun.addEventListener('click',()=>{
     moon.style.right = "4px";
     sun.style.left = "-15px";
-    moon.style.color = "black";
+    
     section.classList.toggle('active-section');
     aside.classList.toggle('active-aside');
     icon.forEach((value)=>{value.style.color ="white";});
+    icon1.forEach((value)=>{value.style.color ="white";});
     html.style.backgroundColor ="#0d0d0d";
     foot.classList.toggle('active-footer');
-    holder.style.backgroundColor ="white";
+    holder.style.backgroundColor ="white"
+    moon.style.color = "black";
 })
